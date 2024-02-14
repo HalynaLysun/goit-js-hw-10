e12074e0154a2bbc0e51dc532a1a32f4c319144e
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-02-14 13:40:29 +0200

    cor

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,9 +1,5 @@
-// Описів у документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
-
-
 
 const inputEl = document.querySelector('#datetime-picker')
 const buttonEl = document.querySelector('button[data-start]')
