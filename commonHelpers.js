9e4a25c81241dd518b48c4d5745214d235321031
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-02-15 12:26:18 +0000

    Deploying to gh-pages from @ HalynaLysun/goit-js-hw-10@53d669ad2c149368f4e8eda2d2871c20429711dc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as e}from"./assets/vendor-2b44ac2e.js";const o=document.querySelector("#datetime-picker");document.querySelector("button[data-start]");const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[1])}};e(o,r);
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{f as t}from"./assets/vendor-2b44ac2e.js";document.querySelector("#datetime-picker");document.querySelector("button[data-start]");const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",o);
 //# sourceMappingURL=commonHelpers.js.map
