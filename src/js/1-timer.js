2218de643e97e4a1eeedff073284f5a03b5644b5
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-02-22 20:59:50 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -18,7 +18,7 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  dateFormat: 'Y-m-d h:m',
+  dateFormat: 'Y-m-d hh:mm',
   onClose(selectedDates) {
     if (selectedDates[0] < options.defaultDate) {
         iziToast.error({
